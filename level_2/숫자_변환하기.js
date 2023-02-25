b15433c045038fb7156b53be48461301db793bc6
Raehyeon Jeong <raehy19@gmail.com>
2023-02-25 14:08:28 +0900

    Solve : level_2 : 숫자 변환하기

--- level_2/숫자_변환하기.js
+++ level_2/숫자_변환하기.js
@@ -1,48 +1,21 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/154538
 
-class node {
-  value;
-  depth;
-  constructor(value, depth, n, y, answerDepthList) {
-    this.value = value;
-    this.depth = depth;
-    if (this.value === y) {
-      if (answerDepthList.length === 0 || answerDepthList[0] > this.depth)
-        answerDepthList.push(this.depth);
-    }
-  }
-
-  addChild(y, n, nodelist, answerDepthList) {
-    if (this.value * 3 <= y)
-      nodelist.push(
-        new node(this.value * 3, this.depth + 1, n, y, answerDepthList)
-      );
-    if (this.value * 2 <= y)
-      nodelist.push(
-        new node(this.value * 2, this.depth + 1, n, y, answerDepthList)
-      );
-    if (this.value + n <= y)
-      nodelist.push(
-        new node(this.value + n, this.depth + 1, n, y, answerDepthList)
-      );
-  }
-}
-
 function solution(x, y, n) {
   if (x === y) return 0;
-  const nodelist = [];
-  const answerDepthList = [];
-  nodelist.push(new node(x, 0, n, y, answerDepthList));
-  for (let i = 0; i < nodelist.length; ++i) {
-    if (
-      answerDepthList.length !== 0 &&
-      answerDepthList[0] < nodelist[i].depth + 2
-    )
-      continue;
-    nodelist[i].addChild(y, n, nodelist, answerDepthList);
+  const dp = {};
+  dp[x] = 0;
+  let layer = [x];
+  while (layer.length) {
+    const nextLayer = [];
+    for (const num of layer) {
+      for (const nextNum of [num + n, num * 2, num * 3]) {
+        if (nextNum > y || dp[nextNum]) continue;
+        if (nextNum === y) return dp[num] + 1;
+        dp[nextNum] = dp[num] + 1;
+        nextLayer.push(nextNum);
+      }
+    }
+    layer = nextLayer;
   }
-  // console.log(answerDepthList);
-  // console.log(nodelist);
-  if (answerDepthList.length === 0) return -1;
-  return answerDepthList[0];
+  return -1;
 }
